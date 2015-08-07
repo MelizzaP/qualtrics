f5ef1a05f5cbd96e5566693e597d30aef285e389
Melissa <melpatenator@gmail.com>
2015-08-07 16:01:10 -0500

    mp3s make everything better

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+  audiojs.events.ready(function() {
+    var as = audiojs.createAll();
+  });
+
+  var clickCount = 0;
+
+  $('.trigger').click(function () {
+    clickCount++;
+    timePlayed = $('.played')
+                  .text()
+                  .split(':')
+                  .map(function(n) {
+                    return parseInt(n);
+                  });
+    timePlayed = timePlayed[0]*60+timePlayed[1];
+    console.log(timePlayed)
+  });
+});
